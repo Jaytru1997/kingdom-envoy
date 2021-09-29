cba17f1ea5d9827f51324c6d934df14aaab9e1ec
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2021-09-30 00:49:19 +0100

    Auto typing section added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="home">
-    <h1>Homepage</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur consectetur adipisci nihil possimus nostrum, culpa nisi earum, tempora nulla facilis obcaecati labore minima cumque in incidunt voluptate necessitatibus velit dicta!</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur consectetur adipisci nihil possimus nostrum, culpa nisi earum, tempora nulla facilis obcaecati labore minima cumque in incidunt voluptate necessitatibus velit dicta!</p>
+  <!-- Introduction Section -->
+  <div class="mx-lg-5 fluid header mb-3">
+    <MDBContainer>
+      <MDBRow>  
+        <MDBCol col="6" md="6" sm="12">
+          <img src="">
+        </MDBCol>
+        <MDBCol col="6" md="6" sm="12" class="auto-type"></MDBCol>
+      </MDBRow>
+    </MDBContainer>
   </div>
+  <!-- Introduction Section Ends -->
+
+  <!-- Service Days Section -->
+
+  <!-- Service Days Section Ends -->
+  
 </template>
 
 <script>
+import { MDBContainer, MDBCol, MDBRow } from 'mdb-vue-ui-kit';
+import { init } from 'ityped';
 
 export default {
   name: 'Home',
-  components: { }
-}
+  components: { 
+    MDBContainer,
+    MDBCol,
+    MDBRow
+  },
+  mounted() {
+    const autoType = document.querySelector('.auto-type')
+
+    init(autoType, { showCursor: false, strings: ['HELLO! ON BEHALF OF THE ENTIRE ASSEMBLY, I WOULD LIKE TO WELCOME YOU TO KINGDOM ENVOY. MY PRIMARY ASSIGNMENT IS TO RAISE TRUE WORSHIPPERS, TRANSFORMING NATIONS. PASTOR MURPHY AKPOVI'] }) 
+  }
+};
 </script>
