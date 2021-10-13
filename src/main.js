aa6e09b81e1e5e8d875897cbe481d4dc5435a88e
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2021-10-13 15:47:42 +0100

    delete frontend of project

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'mdb-vue-ui-kit/css/mdb.min.css'
-import './assets/css/main.css'
-
-createApp(App).use(router).mount('#app')
