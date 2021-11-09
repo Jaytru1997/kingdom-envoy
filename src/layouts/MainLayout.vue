a9f47755c24b4060ef3d3f49d92bcd0aac242e3c
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2021-11-09 14:18:17 +0100

    responsive design

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,39 +1,33 @@
 <template>
-  <q-layout view="hHh lpR fFf">
+  <div class="q-pa-md">
+    <q-toolbar class="bg-dark text-white">
+      <q-avatar>
+        <img src="../assets/img/logo.png">
+      </q-avatar>
 
-    <q-header elevated class="bg-primary text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="../assets/img/logo.png">
-          </q-avatar>
-          KINGDOM ENVOY
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-    <q-footer reveal class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="../assets/img/logo.png">
-          </q-avatar>
-          <div>
-            <!-- footer content goes here-->
-          </div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
-
-  </q-layout>
+      <q-toolbar-title class="menu-tabs">
+        <q-tabs v-model="tab">
+          <q-tab name="about" label="Know us" />
+          <q-tab name="service" label="Services" />
+          <q-tab name="blog" label="Blog" />
+          <q-tab name="contact" label="Prayers & Testimonies" />
+          <q-tab name="live" label="Live Stream" />
+        </q-tabs>
+      </q-toolbar-title>
+      <q-btn flat round dense icon="menu" class="q-mr-sm menu-icon" />
+      <!-- <q-btn flat round dense icon="whatshot" /> -->
+    </q-toolbar>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  
+  setup () {
+    return {
+      tab: ref('')
+    }
+  }
 }
 </script>
