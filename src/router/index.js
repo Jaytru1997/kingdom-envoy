9b84e9266c0c60a0260ce4e71db3e7c75f20d9c9
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2021-11-09 11:29:43 +0100

    template files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import '../css/app.css'
 
 /*
  * If not building with SSR mode, you can
