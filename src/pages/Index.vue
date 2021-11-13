d4377c172ee904329969db1e734edcaa8e156a53
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2021-11-13 22:23:33 +0100

    carousel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
-  </q-page>
+    > -->
+    <!--Carousel-->
+    <div class="">
+      <q-carousel
+        animated
+        v-model="slide"
+        arrows
+        navigation
+        infinite
+      >
+        <q-carousel-slide :name="1" img-src="../assets/img/IMG_6279.png">
+          <div class="absolute-center custom-caption text-center text-white">
+            <div class="text-h4">Lead Pastor</div>
+            <div class="text-subtitle1">The Reverend Dr. Murphy Akpovi</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" img-src="../assets/img/online.jpg" />
+        <q-carousel-slide :name="3" img-src="../assets/img/IMG_6315.png" />
+        <q-carousel-slide :name="4" img-src="../assets/img/online.jpg" />
+      </q-carousel>
+    </div>
+    
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { ref } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      slide: ref(1)
+    }
+  }
 })
 </script>
